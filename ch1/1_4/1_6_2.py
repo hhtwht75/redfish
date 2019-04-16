3ed063a35fcec57f7dda4b54d6e43cf5d026cd67
Jung-Hun Park <hhtwht75@gmail.com>
2019-04-16 08:34:35 -0400

    ch1 & ch2 commit

---
+++ ch1/1_4/1_6_2.py
@@ -0,0 +1,18 @@
+#page 43
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#preparing data
+x = np.arange(0, 6, 0.1)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+#drawing graph
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title('sin & cos')
+plt.legend()
+plt.show()
---
+++ ch1/1_4/man.py
@@ -0,0 +1,19 @@
+#page 35
+
+class Man:
+    
+    def __init__(self, name):
+        self.test = name
+        self.test2 = "dear " + name
+        print("Initialized!")
+
+    def hello(self):
+        print("Hello " + self.test + "!")
+
+    def goodbye(self):
+        print("Good-bye " + self.test2 + "!")
+
+m = Man("David")
+m.hello()
+m.goodbye()
+
