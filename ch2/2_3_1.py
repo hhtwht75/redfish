3508ba64dbb57ee8367efb70c33b83f88f35b5a9
Jung-Hun Park <hhtwht75@gmail.com>
2019-04-17 03:47:41 -0400

    XOR implement

--- ch2/2_3_1.py
+++ ch2/2_3_1.py
@@ -1,5 +1,8 @@
 #page 51
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 def AND(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
     tmp = x1*w1 + x2*w2
--- ch2/2_3_3_gates.py
+++ ch2/2_3_3_gates.py
@@ -1,5 +1,8 @@
 #page 53
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 def AND(x1, x2):
 
     x = np.array([x1,x2])
