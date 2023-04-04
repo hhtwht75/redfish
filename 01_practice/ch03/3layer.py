8d7b7e129071fce06329bd9c174f0763f594a630
JH94_Surface <hhtwht75@gmail.com>
2023-04-04 19:26:39 +0900

    act func에서 출력 지움

---
+++ 01_practice/ch03/3layer.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib as plt
+from act_func import *
+
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+
+A1 = np.dot(X, W1) + B1
--- 01_practice/ch03/act_func.py
+++ 01_practice/ch03/act_func.py
@@ -9,11 +9,3 @@ def sigmoid(x):
 
 def relu(x):
     return np.maximum(0, x)
-
-x = np.arange(-5.0, 5.0, 0.1)
-y = relu(x)
-
-plt.plot(x, y)
-plt.ylim(-0.1, 1.1)
-plt.show()
-
