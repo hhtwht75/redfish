3f18cba8c26eabd76041901c18d9c85909ac30c8
JH94_Surface <hhtwht75@gmail.com>
2023-04-04 20:39:31 +0900

    MNIST 전까지 완료했음.

--- 01_practice/ch03/act_func.py
+++ 01_practice/ch03/act_func.py
@@ -9,3 +9,10 @@ def sigmoid(x):
 
 def relu(x):
     return np.maximum(0, x)
+
+def softmax(x):
+    max_x = np.max(x)
+    exp_x = np.exp(x - max_x)
+    sum_exp_x = np.sum(exp_x)
+    y = exp_x / sum_exp_x
+    return y
